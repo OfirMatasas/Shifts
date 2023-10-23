0f50bc0d53974da8535a77530e906ec698763a3f
ofirmatasas <ofir.matasas@gmail.com>
2023-10-23 14:28:06 +0300

    Convert code to functions

--- main.py
+++ main.py
@@ -8,10 +8,11 @@ hard_shifts_hours = [("01:00", "04:00"), ("04:00", "07:00")]
 shifts_space = {key: 0 for key in team_members}
 
 # Define the days off
-first_day_off = [("Sunday", ("22:00", "01:00")), ("Tuesday", ("22:00", "01:00")), ["Ofer", "Nissan"]]
-second_day_off = [("Tuesday", ("22:00", "01:00")), ("Thursday", ("22:00", "01:00")), ["Or", "Michal"]]
-third_day_off = [("Thursday", ("22:00", "01:00")), ("Saturday", ("22:00", "01:00")), ["Matasas", "Pavel"]]
-days_off = [first_day_off, second_day_off, third_day_off]
+days_off = [
+    [("Sunday", ("22:00", "01:00")), ("Tuesday", ("22:00", "01:00")), ["Ofer", "Nissan"]],
+    [("Tuesday", ("22:00", "01:00")), ("Thursday", ("22:00", "01:00")), ["Or", "Michal"]],
+    [("Thursday", ("22:00", "01:00")), ("Saturday", ("22:00", "01:00")), ["Matasas", "Pavel"]]
+]
 team_members_on_day_off = []
 on_call = []
 
@@ -22,77 +23,120 @@ shifts = [(f"{hour:02}:00", f"{(hour + 3) % 24:02}:00") for hour in range(1, 24,
 days_of_week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
 schedule = {day: {} for day in days_of_week}
 
-for day in days_of_week:
+
+def remove_team_members_if_day_off_is_on(day, shift):
+    '''
+    Check if the current shift is a start of a day off. If so - add the team members to the team_members_on_day_off list and remove them from the team_members list
+    '''
+
+    for day_off in days_off:
+        if (day, shift) == day_off[0]:
+            for member in day_off[2]:
+                if member in team_members:
+                    team_members_on_day_off.append(member)
+                    shifts_space.pop(member)
+                    hard_shifts_count.pop(member)
+                    team_members.remove(member)
+
+
+def add_team_members_if_day_off_is_over(day, shift):
+    '''
+    Check if the current shift is an end of a day off. If so - add the team members back to the team_members list and remove them from the team_members_on_day_off list
+    '''
+
+    global team_members_on_day_off
+
+    for day_off in days_off:
+        if (day, shift) == day_off[1]:
+            team_members.reverse()
+
+            for member in day_off[2]:
+                if member not in team_members:
+                    team_members.append(member)
+                    shifts_space[member] = 0
+                    hard_shifts_count[member] = 0
+
+            team_members.reverse()
+            team_members_on_day_off = []
+
+
+def set_shift_to_relevant_team_member(day, shift):
+    '''
+    Set the current shift to the team member with the maximum amount of shifts space, as long as they didn't have 2 hard shifts already earlier this week
+    '''
+    if shift in hard_shifts_hours:
+        member_index = 0
+        while True:
+            if hard_shifts_count[team_members[member_index]] == 2:
+                member_index += 1
+            else:
+                break
+
+        hard_shifts_count[team_members[member_index]] += 1
+        schedule[day][shift] = team_members.pop(member_index)
+    else:
+        schedule[day][shift] = team_members.pop(0)
+
+
+def update_shift_space(day, shift):
+    '''
+    Update shifts space for each team member
+    '''
+
+    shifts_space[schedule[day][shift]] = -1
+    for member in team_members:
+        shifts_space[member] += 1
+
+
+def set_on_call_for_team_member(day, shift):
+    '''
+    Set a team member to be on call according to his shift space and past on-call duties
+    '''
+
+    if shift == ("22:00", "01:00"):
+        member_index = 0
+        while True:
+            if on_call_count[team_members[member_index]] == 1:
+                member_index += 1
+            else:
+                break
+
+        on_call_count[team_members[member_index]] += 1
+        on_call.append(team_members[member_index])
+        shifts_space[team_members[member_index]] = -4
+
+
+def print_shifts_schedule():
+    table = [["Shift"] + days_of_week]
+
     for shift in shifts:
+        table.append([f"{shift[0]} - {shift[1]}"] + [schedule[day][shift] for day in days_of_week])
 
-        # Check if the current shift is a start of a day off. If so - add the team members to the team_members_on_day_off list and remove them from the team_members list
-        for day_off in days_off:
-            if (day, shift) == day_off[0]:
-                for member in day_off[2]:
-                    if member in team_members:
-                        team_members_on_day_off.append(member)
-                        shifts_space.pop(member)
-                        hard_shifts_count.pop(member)
-                        team_members.remove(member)
+    table.append([f"On Call"] + [member for member in on_call])
 
-        # Check if the current shift is an end of a day off. If so - add the team members back to the team_members list and remove them from the team_members_on_day_off list
-        for day_off in days_off:
-            if (day, shift) == day_off[1]:
-                team_members.reverse()
+    print(tabulate(table, headers="firstrow", tablefmt="pretty"))
 
-                for member in day_off[2]:
-                    if member not in team_members:
-                        team_members.append(member)
-                        shifts_space[member] = 0
-                        hard_shifts_count[member] = 0
 
-                team_members.reverse()
-                team_members_on_day_off = []
+def build_shifts_schedule():
+    for day in days_of_week:
+        for shift in shifts:
 
-        team_members.sort(key=lambda member: shifts_space[member], reverse=True)
+            remove_team_members_if_day_off_is_on(day, shift)
+            add_team_members_if_day_off_is_over(day, shift)
 
-        # Get the team member with the maximum amount of shifts space
+            team_members.sort(key=lambda member: shifts_space[member], reverse=True)
 
-        # Set the current shift to the team member with the maximum amount of shifts space
-        # But, if the current shift is a hard shift, set it to the team member with the maximum amount of shifts space, as long as he didn't have a 2 hard shifts already earlier this week
-        if shift in hard_shifts_hours:
-            member_index = 0
-            while True:
-                if hard_shifts_count[team_members[member_index]] == 2:
-                    member_index += 1
-                else:
-                    break
+            set_shift_to_relevant_team_member(day, shift)
 
-            hard_shifts_count[team_members[member_index]] += 1
-            schedule[day][shift] = team_members.pop(member_index)
-        else:
-            schedule[day][shift] = team_members.pop(0)
+            update_shift_space(day, shift)
 
-        # Update the shifts space for each team member
-        shifts_space[schedule[day][shift]] = -1
-        for member in team_members:
-            shifts_space[member] += 1
+            # Add the team member back to the team_members list
+            team_members.append(schedule[day][shift])
 
-        # Add the team member back to the team_members list
-        team_members.append(schedule[day][shift])
+            set_on_call_for_team_member(day, shift)
 
-        if shift == ("22:00", "01:00"):
-            member_index = 0
-            while True:
-                if on_call_count[team_members[member_index]] == 1:
-                    member_index += 1
-                else:
-                    break
 
-            on_call_count[team_members[member_index]] += 1
-            on_call.append(team_members[member_index])
-            shifts_space[team_members[member_index]] = -4
+if __name__ == "__main__":
 
-# Print the schedule using tabulate
-table = [["Shift"] + days_of_week]
-for shift in shifts:
-    table.append([f"{shift[0]} - {shift[1]}"] + [schedule[day][shift] for day in days_of_week])
-
-table.append([f"On Call"] + [member for member in on_call])
-
-print(tabulate(table, headers="firstrow", tablefmt="pretty"))
+    build_shifts_schedule()
+    print_shifts_schedule()
