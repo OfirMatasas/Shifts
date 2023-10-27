0ab335b1ad68c938286c3db92f986a53cb31e51b
ofirmatasas <ofir.matasas@gmail.com>
2023-10-27 18:37:59 +0300

    Make some consmetic changes

--- main.py
+++ main.py
@@ -1,28 +1,57 @@
 from tabulate import tabulate
 from csv import writer, reader
 
-# List of team members
-team_members = ["Hadar", "Matasas", "Ofer", "Nissan", "Or", "Michal", "Eylon", "Pavel", "Ido"]
+# Constants
+SUNDAY = "Sunday"
+MONDAY = "Monday"
+TUESDAY = "Tuesday"
+WEDNESDAY = "Wednesday"
+THURSDAY = "Thursday"
+FRIDAY = "Friday"
+SATURDAY = "Saturday"
+SHIFT = "Shift"
+ON_CALL = "On Call"
+ON_CALL_SHIFT = ("22:00", "01:00")
+MAX_HARD_SHIFTS_PER_WEEK = 2
+DAY_OFF_START_SHIFT_INDEX = 0
+DAY_OFF_END_SHIFT_INDEX = 1
+DAY_OFF_TEAM_MEMBERS_INDEX = 2
+SPACE_ON_CALL = -4
+
+# Team members' names
+HADAR = "Hadar"
+MATASAS = "Matasas"
+OFER = "Ofer"
+NISSAN = "Nissan"
+OR = "Or"
+MICHAL = "Michal"
+EYLON = "Eylon"
+PAVEL = "Pavel"
+IDO = "Ido"
+
+# Define the shifts (24 hours divided into 3-hour shifts)
+shifts = [(f"{hour:02}:00", f"{(hour + 3) % 24:02}:00") for hour in range(1, 24, 3)]
+
+# List of team members and their shifts space
+team_members = [HADAR, MATASAS, OFER, NISSAN, OR, MICHAL, EYLON, PAVEL, IDO]
 hard_shifts_count = {key: 0 for key in team_members}
 on_call_count = {key: 0 for key in team_members}
 hard_shifts_hours = [("01:00", "04:00"), ("04:00", "07:00")]
 shifts_space = {key: 0 for key in team_members}
 
+# Define the work days
+work_days = [SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY]
+
 # Define the days off
 days_off = [
-    [("Sunday", ("22:00", "01:00")), ("Tuesday", ("22:00", "01:00")), ["Ofer", "Nissan"]],
-    [("Tuesday", ("22:00", "01:00")), ("Thursday", ("22:00", "01:00")), ["Or", "Michal"]],
-    [("Thursday", ("22:00", "01:00")), ("Saturday", ("22:00", "01:00")), ["Matasas", "Pavel"]]
+    # [(START_DAY, (SHIFT_START, SHIFT_END)), (END_DAY, (SHIFT_START, SHIFT_END)), [TEAM_MEMBERS]],
+    # [(MONDAY, ("22:00", "01:00")), (TUESDAY, ("22:00", "01:00")), [HADAR, MATASAS]],
 ]
 team_members_on_day_off = []
 on_call = []
 
-# Define the shifts (24 hours divided into 3-hour shifts)
-shifts = [(f"{hour:02}:00", f"{(hour + 3) % 24:02}:00") for hour in range(1, 24, 3)]
-
 # Create a schedule for 7 days
-days_of_week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
-schedule = {day: {} for day in days_of_week}
+schedule = {day: {} for day in work_days}
 
 
 def remove_team_members_if_day_off_is_on(day, shift):
@@ -31,8 +60,8 @@ def remove_team_members_if_day_off_is_on(day, shift):
     '''
 
     for day_off in days_off:
-        if (day, shift) == day_off[0]:
-            for member in day_off[2]:
+        if (day, shift) == day_off[DAY_OFF_START_SHIFT_INDEX]:
+            for member in day_off[DAY_OFF_TEAM_MEMBERS_INDEX]:
                 if member in team_members:
                     team_members_on_day_off.append(member)
                     shifts_space.pop(member)
@@ -48,10 +77,10 @@ def add_team_members_if_day_off_is_over(day, shift):
     global team_members_on_day_off
 
     for day_off in days_off:
-        if (day, shift) == day_off[1]:
+        if (day, shift) == day_off[DAY_OFF_END_SHIFT_INDEX]:
             team_members.reverse()
 
-            for member in day_off[2]:
+            for member in day_off[DAY_OFF_TEAM_MEMBERS_INDEX]:
                 if member not in team_members:
                     team_members.append(member)
                     shifts_space[member] = 0
@@ -65,18 +94,18 @@ def set_shift_to_relevant_team_member(day, shift):
     '''
     Set the current shift to the team member with the maximum amount of shifts space, as long as they didn't have 2 hard shifts already earlier this week
     '''
+
+    member_index = 0
+
     if shift in hard_shifts_hours:
-        member_index = 0
         while True:
-            if hard_shifts_count[team_members[member_index]] == 2:
-                member_index += 1
-            else:
+            if 0 <= hard_shifts_count[team_members[member_index]] < MAX_HARD_SHIFTS_PER_WEEK:
                 break
+            member_index += 1
 
         hard_shifts_count[team_members[member_index]] += 1
-        schedule[day][shift] = team_members.pop(member_index)
-    else:
-        schedule[day][shift] = team_members.pop(0)
+
+    schedule[day][shift] = team_members.pop(member_index)
 
 
 def update_shift_space(day, shift):
@@ -94,32 +123,32 @@ def set_on_call_for_team_member(shift):
     Set a team member to be on call according to his shift space and past on-call duties
     '''
 
-    if shift == ("22:00", "01:00"):
+    if shift == ON_CALL_SHIFT:
         member_index = 0
-        while True:
-            if on_call_count[team_members[member_index]] == 1:
-                member_index += 1
-            else:
+
+        while member_index < len(team_members):
+            if on_call_count[team_members[member_index]] == 0:
                 break
+            member_index += 1
 
         on_call_count[team_members[member_index]] += 1
         on_call.append(team_members[member_index])
-        shifts_space[team_members[member_index]] = -4
+        shifts_space[team_members[member_index]] = SPACE_ON_CALL
 
 
 def print_shifts_schedule():
-    table = [["Shift"] + days_of_week]
+    table = [[SHIFT] + work_days]
 
     for shift in shifts:
-        table.append([f"{shift[0]} - {shift[1]}"] + [schedule[day][shift] for day in days_of_week])
+        table.append([f"{shift[0]} - {shift[1]}"] + [schedule[day][shift] for day in work_days])
 
-    table.append([f"On Call"] + [member for member in on_call])
+    table.append([f"{ON_CALL}"] + [member for member in on_call])
 
     print(tabulate(table, headers="firstrow", tablefmt="pretty"))
 
 
 def build_shifts_schedule():
-    for day in days_of_week:
+    for day in work_days:
         for shift in shifts:
 
             remove_team_members_if_day_off_is_on(day, shift)
@@ -145,19 +174,23 @@ def initialize_shift_according_to_old_schedule_hard_coded():
     global on_call, hard_shifts_count, shifts_space
 
     # Initialize on-call duties
-    # For instance: on_call_count["Matasas"] = 1
+    # For instance: on_call_count[MATASAS] = 1
 
     # Initialize shifts space
     # For instance:
-    #   shifts_space["Matasas"] = 2
-    #   shifts_space["Ofer"] = 3
+    #   shifts_space[MATASAS] = 2
+    #   shifts_space[PAVEL] = 1
+    #   shifts_space[OFER] = 3
 
     # Initialize hard shifts count
     # For instance:
-    #   hard_shifts_count["Matasas"] = 2
-    #   hard_shift_count["Ofer"] = 1
+    #   hard_shifts_count[MATASAS] = 2
+    #   hard_shifts_count[OR] = 1
 
-    pass
+    # Remove team members which are on off day
+    # For instance:
+    #   team_members.remove(PAVEL)
+    #   team_members.remove(MICHAL)
     
 
 def write_to_csv_file():
@@ -165,12 +198,12 @@ def write_to_csv_file():
     
     with open("schedule.csv", "w", newline="") as file:
         csv_writer = writer(file)
-        csv_writer.writerow(["Shift"] + days_of_week)
+        csv_writer.writerow([SHIFT] + work_days)
 
         for shift in shifts:
-            csv_writer.writerow([f"{shift[0]} - {shift[1]}"] + [schedule[day][shift] for day in days_of_week])
+            csv_writer.writerow([f"{shift[0]} - {shift[1]}"] + [schedule[day][shift] for day in work_days])
 
-        csv_writer.writerow([f"On Call"] + [member for member in on_call])
+        csv_writer.writerow([f"{ON_CALL}"] + [member for member in on_call])
 
 
 def load_old_schedule_fron_csv_file_and_initialize_shift_according_to_old_schedule():
@@ -180,21 +213,21 @@ def load_old_schedule_fron_csv_file_and_initialize_shift_according_to_old_schedu
         csv_reader = reader(file)
 
         for row in csv_reader:
-            if row[0] == "Shift":
+            if row[0] == SHIFT:
                 continue
 
-            if row[0] == "On Call":
+            if row[0] == ON_CALL:
                 on_call = row[1:]
                 continue
 
-            for day in days_of_week:
-                schedule[day][tuple(row[0].split(" - "))] = row[days_of_week.index(day) + 1]
+            for day in work_days:
+                schedule[day][tuple(row[0].split(" - "))] = row[work_days.index(day) + 1]
 
     # initialize_shift_according_to_old_schedule()
         
 
 if __name__ == "__main__":
-    load_old_schedule_fron_csv_file_and_initialize_shift_according_to_old_schedule()
+    # load_old_schedule_fron_csv_file_and_initialize_shift_according_to_old_schedule()
     initialize_shift_according_to_old_schedule_hard_coded()
     build_shifts_schedule()
     print_shifts_schedule()
